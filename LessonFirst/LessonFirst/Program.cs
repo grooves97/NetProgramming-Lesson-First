6b64cbf6e8139febcb5ce7be83835b62e7e0204a
Аслан Нурсеитов <grooves97@gmail.com>
2019-06-18 11:52:54 +0600

    Dns class, SocketLesson

---
+++ LessonFirst/LessonFirst/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace LessonFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string compName = Dns.GetHostName();
+
+            Console.WriteLine($"This host name: {compName}");
+
+            IPHostEntry ipList = Dns.GetHostByName(compName);
+
+            foreach (var a in ipList.AddressList)
+            {
+                Console.WriteLine($"Address list {a.ToString()}");
+            }
+
+            Console.WriteLine("Введите имя сетевого ресурса");
+
+            compName = Console.ReadLine();
+            ipList = Dns.GetHostByName(compName);
+
+            foreach (var item in ipList.AddressList)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+            Socket socket = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream,
+                ProtocolType.Tcp);
+            try
+            {
+                socket.Connect(ipList.AddressList[0], 443);
+                if (socket.Connected)
+                {
+                    string httpRequest = "GET/HTTP 1.0/";
+                    socket.Send(Encoding.ASCII.GetBytes(httpRequest));
+                    byte[] buf = new byte[1024*4];
+                    socket.Receive(buf);
+                    Console.WriteLine($"Received: {Encoding.UTF8.GetString(buf)}");
+                }
+                else
+                {
+                    Console.WriteLine("Error: connected fail!");
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"Упс что-то пошло не так{exception.Message}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LessonFirst/ServerApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ServerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static void Server(IPAddress ipAddr, int port)
+        {
+            //Сокет сервера
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream, ProtocolType.Tcp);
+            //Связывание сокета сервера с конечной точкой:
+            //IP-адрес + порт
+            IPEndPoint serverEndPoint = new IPEndPoint(ipAddr, port);
+            serverSocket.Bind(serverEndPoint);
+            //Публикация сокета сервера в сети - открытие доступа к серверу
+            serverSocket.Listen(100);
+            //Ожидание вязи со стороны
+            Socket clientSocket = serverSocket.Accept();
+            //передать сообщение удаленному клиенту
+            //clientSocket.Send();
+            //принять сообщение от клиента
+            //clientSocket.Receive();
+        }
+    }
+}
