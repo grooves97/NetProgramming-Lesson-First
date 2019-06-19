1cb84485b93176b934c2d6920e24b647e53e7424
Аслан Нурсеитов <grooves97@gmail.com>
2019-06-19 12:02:23 +0600

    Создание клиент сервера. Дописывать

--- LessonFirst/ServerApp/Program.cs
+++ LessonFirst/ServerApp/Program.cs
@@ -12,8 +12,17 @@ namespace ServerApp
     {
         static void Main(string[] args)
         {
+            IPAddress ip = IPAddress.Parse("0.0.0.0"); //мы принимаем всех user
+            //IPAddress ip = IPAddress.Parse("127.0.0.1"); это локал хост /ping -4 localhost
+            //IPAddress ip = IPAddress.Parse("192.168.56.1");//mask 255.255.0.0
 
+            int port = 12345;
+            Server(ip,port);
         }
+
+        //0 - 1000 - ftp, http, dns
+        //1000 - 10000 user ports
+        // > 10000 - temporary port
 
         static void Server(IPAddress ipAddr, int port)
         {
@@ -26,12 +35,39 @@ namespace ServerApp
             serverSocket.Bind(serverEndPoint);
             //Публикация сокета сервера в сети - открытие доступа к серверу
             serverSocket.Listen(100);
-            //Ожидание вязи со стороны
-            Socket clientSocket = serverSocket.Accept();
-            //передать сообщение удаленному клиенту
-            //clientSocket.Send();
-            //принять сообщение от клиента
-            //clientSocket.Receive();
+
+            while (true)
+            {
+                //Ожидание вязи со стороны
+                Socket clientSocket = serverSocket.Accept();
+                Console.WriteLine($"Client connected: {clientSocket.RemoteEndPoint.ToString()}");
+
+                try
+                {
+                    while (clientSocket.Connected)
+                    {
+                        //Протокол работы эхо-сервера (порт 7 echo)
+                        byte[] buf = new byte[1024];
+                        //принять сообщение от клиента
+                        int recSize = clientSocket.Receive(buf);
+
+                        if (recSize == 0) break;
+
+                        Console.WriteLine($"Received {recSize} bytes");
+                        Console.WriteLine(Encoding.UTF8.GetString(buf));
+                        //передать получное сообщение удаленному клиенту
+                        int sendSize = clientSocket.Send(buf, SocketFlags.None);
+                        Console.WriteLine($"Send to client {sendSize} bytes");
+
+                    }
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine($"Error: {exception.Message}");
+                }
+
+            }
+
         }
     }
 }
---
+++ LessonFirst/ServerWPF/MainWindow.xaml.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace ServerWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bool buttonIsStart;
+        private TcpListener serverSock;
+        private Thread serverThread;
+        private ManualResetEvent eventStop;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //eventStop = new ManualResetEvent;
+            IPAddresComboBox.Items.Add("0.0.0.0");
+            IPAddresComboBox.Items.Add("127.0.0.1");
+            IPHostEntry entryServer =  Dns.GetHostEntry(Dns.GetHostName());
+
+            foreach (var i in entryServer.AddressList)
+            {
+                IPAddresComboBox.Items.Add(i.ToString());
+            }
+
+            buttonIsStart = false;
+            serverSock = null;
+            serverThread = null;
+        }
+        /// <summary>
+        /// Кнопка включения выключения чего-то
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void StartButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (!buttonIsStart) //запуск сервера
+            {
+                try
+                {
+                    serverSock = new TcpListener(IPAddress.Parse(IPAddresComboBox.Text), int.Parse(PortTextBox.Text));
+
+                    serverSock.Start();
+                    serverThread = new Thread(ServerThreadProcess);
+                    serverThread.Start(serverSock);
+                    buttonIsStart = !buttonIsStart;
+                    StartButton.Content = "Stop";
+                }
+                catch (Exception exception)
+                {
+                    MessageBox.Show(exception.Message,"Error", MessageBoxButton.OK, MessageBoxImage.Stop);
+                }
+            }
+            else
+            {
+                //Нужен объект синхронизации с потоком сервера
+
+            }
+
+          
+        }
+        /// <summary>
+        /// Поток сервера
+        /// </summary>
+        /// <param name="obj"></param>
+        private void ServerThreadProcess(object obj)
+        {
+            TcpListener serverSocket = (TcpListener)obj;
+            while (true)
+            {
+                //Вариант первый
+                //TcpClient client = serverSocket.AcceptTcpClient();
+                //Обработка клиента в поле потоков
+                //ThreadPool.QueueUserWorkItem(ThreadClient, client);
+                //Вариант второй
+                IAsyncResult asyncResult = serverSock.BeginAcceptSocket(AsyncServerProc, serverSock);
+                asyncResult.AsyncWaitHandle.WaitOne();
+
+                //if (eventStop.Wa)
+                //{
+
+                //}
+            }
+        }
+
+        private void AsyncServerProc(IAsyncResult ar)
+        {
+            TcpListener serverSock = (TcpListener)ar.AsyncState;
+
+            TcpClient client = serverSock.EndAcceptTcpClient(ar);
+            //Запуск потока клиента в пуле потоков
+            //ThreadPool.QueueUserWorkItem(ThreadClient, client);
+
+        }
+
+        /// <summary>
+        /// Поток обслуживания клиента
+        /// </summary>
+        /// <param name="obj"></param>
+        private void ThreadClientProcess(object obj)
+        {
+            TcpClient client = (TcpClient)obj;
+            //Работа с клиентом через объектов TcpClient
+            //Определить общение с клиентом
+        }
+    }
+}
